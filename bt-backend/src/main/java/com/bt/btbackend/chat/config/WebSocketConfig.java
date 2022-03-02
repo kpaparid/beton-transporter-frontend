7176fe313dbd99ea309ee5d277279479a3b6383b
DESKTOP-UVL41M2\i7 <kpaparid@gmail.com>
2022-03-02 05:48:28 +0100

    no message

--- bt-backend/src/main/java/com/bt/btbackend/chat/config/WebSocketConfig.java
+++ bt-backend/src/main/java/com/bt/btbackend/chat/config/WebSocketConfig.java
@@ -33,8 +33,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry
                 .addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:3000/")
-//                .setAllowedOrigins("https://modest-morse-7f9546.netlify.app")
+//                .setAllowedOrigins("http://localhost:3000/")
+                .setAllowedOrigins("https://modest-morse-7f9546.netlify.app")
 //                .setAllowedOrigins(restSecProps.getAllowedOrigins())
                 .withSockJS();
     }
